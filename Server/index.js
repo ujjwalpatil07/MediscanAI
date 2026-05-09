5247b68ebd79ac1505a7bd841ccfe97408331263
Ujjwal patil <ujjwalpatil0011.55@gmail.com>
2026-05-09 13:32:45 +0530

    .

--- Server/index.js
+++ Server/index.js
@@ -25,7 +25,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
